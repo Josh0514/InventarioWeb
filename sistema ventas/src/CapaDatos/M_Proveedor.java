8db545eeda28fc70364dc65acb8b6b8dabcdd9f4
joshu <joshu@Joshua>
2023-04-21 18:55:49 -0600

    Nueva Version del Proyeto GNC

---
+++ sistema ventas/src/CapaDatos/M_Proveedor.java
@@ -0,0 +1,63 @@
+package CapaDatos;
+
+public class M_Proveedor {
+    private String idprovedor;
+    private String razonsocial;
+    private String ruc;
+    private String telefono;
+    private String direccion;
+
+    public M_Proveedor() {
+    }
+
+    public M_Proveedor(String idprovedor, String razonsocial, String ruc, String telefono, String direccion) {
+        this.idprovedor = idprovedor;
+        this.razonsocial = razonsocial;
+        this.ruc = ruc;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getIdprovedor() {
+        return idprovedor;
+    }
+
+    public void setIdprovedor(String idprovedor) {
+        this.idprovedor = idprovedor;
+    }
+
+    public String getRazonsocial() {
+        return razonsocial;
+    }
+
+    public void setRazonsocial(String razonsocial) {
+        this.razonsocial = razonsocial;
+    }
+
+    public String getRuc() {
+        return ruc;
+    }
+
+    public void setRuc(String ruc) {
+        this.ruc = ruc;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+    
+}
